f4b4805753262adec1ec876fe946c4059fb182b9
Andrew <kpi_lithium@ukr.net>
2016-08-16 10:38:34 +0300

    DB connection

--- common/config/main.php
+++ common/config/main.php
@@ -5,6 +5,13 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+		 'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=nf227389.mysql.ukraine.com.ua;dbname=nf227389_db',
+            'username' => 'nf227389_db',
+            'password' => 'znAhyJ7F',
+            'charset' => 'utf8',
+        ],
     ],
     'modules' => [
         'redactor' => [
