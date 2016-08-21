215d74f10acf280ee38bf0b28bac009190577cff
Andrew <kpi_lithium@ukr.net>
2016-08-21 12:05:39 +0300

    Index.php

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -53,45 +53,4 @@ $this->title = 'Таращанська РДА';
 </div>
 
 
-<div class="col-sm-4 col-md-5 ">
-    <div class="main_header">
-        <h3 class="text-info text-uppercase"><span class="glyphicon glyphicon-book"></span>&nbsp;&nbsp;Новини Київщини</h3>
-    </div>
-    <hr>
-    <div class="row vertical-divider-right">
-        <div class="col-md-6">
-            <img width="100%" src="<?= \yii\helpers\Url::to("@web/img/rok211.jpg") ?>"/>
-            <h4>Some article's header</h4>
-            <p> >Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Accusamus aliquid autem molestiae nemo nostrum quam voluptatem.
-                Consequatur deserunt, doloribus, enim id iste, molestias omnis
-                quibusdam rerum sit totam veritatis voluptas.</p>
-        </div>
-        <div class="col-md-6">
-            <img width="100%" src="<?= \yii\helpers\Url::to("@web/img/rok211.jpg") ?>"/>
-            <h4>Some article's header</h4>
-            <p> >Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Accusamus aliquid autem molestiae nemo nostrum quam voluptatem.
-                Consequatur deserunt, doloribus, enim id iste, molestias omnis
-                quibusdam rerum sit totam veritatis voluptas.</p>
-        </div>
-    </div>
-    <div class="row vertical-divider-right">
-        <div class="col-md-6">
-            <img width="100%" src="<?= \yii\helpers\Url::to("@web/img/rok211.jpg") ?>"/>
-            <h4>Some article's header</h4>
-            <p> >Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Accusamus aliquid autem molestiae nemo nostrum quam voluptatem.
-                Consequatur deserunt, doloribus, enim id iste, molestias omnis
-                quibusdam rerum sit totam veritatis voluptas.</p>
-        </div>
-        <div class="col-md-6">
-            <img width="100%" src="<?= \yii\helpers\Url::to("@web/img/rok211.jpg") ?>"/>
-            <h4>Some article's header</h4>
-            <p> >Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Accusamus aliquid autem molestiae nemo nostrum quam voluptatem.
-                Consequatur deserunt, doloribus, enim id iste, molestias omnis
-                quibusdam rerum sit totam veritatis voluptas.</p>
-        </div>
-    </div>
-</div>
+
