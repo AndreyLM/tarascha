01cf43ea269fde048c490a0abff4aaf6cbdc92c0
Andrew <kpi_lithium@ukr.net>
2016-08-25 13:55:57 +0300

    menu x2

--- frontend/views/layouts/partial/footer.php
+++ frontend/views/layouts/partial/footer.php
@@ -11,7 +11,7 @@
         </div>
         <div class="col-md-2"><p align="center"><img src="<?= Url::to("@web/img/gerb.png") ?>"></p></div>
         <div class="col-md-5">
-            <p class="text-left text-uppercase text_footer">09500, 1,st.Goroyiv Chornobilya, Kiev Region<br>
+            <p class="text-left text-uppercase text_footer">09500, 1,st.GEROYIV CHORNOBILYA, Kiev Region<br>
                 ph./fax: (04566) 5-15-83, 5-14-44<br>
                 10rda-tarascha@ukr.net</p></div>
     </div>
