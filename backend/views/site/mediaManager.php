a65d4dd7fbfb83c891fdeeba1e1500864ddfa9a3
Andrew <kpi_lithium@ukr.net>
2016-09-14 14:55:01 +0300

    File manager height

--- backend/views/site/mediaManager.php
+++ backend/views/site/mediaManager.php
@@ -4,8 +4,9 @@ use mihaildev\elfinder\ElFinder;
 use yii\web\JsExpression;
 
 $this->title = "File manager";
-
-echo ElFinder::widget([
+?>
+<div class="file-manager">
+<?= ElFinder::widget([
     'language' => 'ru',
     'controller' => 'elfinder', // вставляем название контроллера, по умолчанию равен elfinder
     'path' => '', // будет открыта папка из настроек контроллера с добавлением указанной под деритории
@@ -19,4 +20,5 @@ echo ElFinder::widget([
             'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
         ], // фильтр файлов, можно задать массив фильтров https://github.com/Studio-42/elFinder/wiki/Client-configuration-options#wiki-onlyMimes
     'callbackFunction' => new JsExpression('function(file, id){}') // id - id виджета
-]);
+]);?>
+</div>
