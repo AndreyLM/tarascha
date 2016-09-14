a5cb68ab774fd99e3a48e43adcf25464a4f3b4e0
Andrew <kpi_lithium@ukr.net>
2016-09-14 17:19:28 +0300

    New color map

--- frontend/views/layouts/partial/map.php
+++ frontend/views/layouts/partial/map.php
@@ -4,52 +4,52 @@ use yii\helpers\Url;
 <div class="map-cover">
     <div class="map">
         <img class="map-img" data-href="<?=Url::to(['/site/article', 'slug' => 'tarasanska-miska-rada'])?>"
-             id="img1" src="http://tarashcha-rda.gov.ua/frontend/web/map_imgs/1.png" data-toggle="tooltip" title="Таращанська міська рада"/>
+             id="img1" src="http://tarashcha-rda.gov.ua/frontend/web/map_imgs/new/1.png" data-toggle="tooltip" title="Таращанська міська рада"/>
         <img class="map-img" data-href="<?=Url::to(['/site/article', 'slug' => 'kirdanivska-silska-rada'])?>"
-             id="img2" src="http://tarashcha-rda.gov.ua/frontend/web/map_imgs/2.png" data-toggle="tooltip" title="Кирданівська сільська рада"/>
+             id="img2" src="http://tarashcha-rda.gov.ua/frontend/web/map_imgs/new/2.png" data-toggle="tooltip" title="Кирданівська сільська рада"/>
         <img class="map-img" data-href="<?=Url::to(['/site/article', 'slug' => 'krivanska-silska-rada'])?>"
-             id="img3" src="http://tarashcha-rda.gov.ua/frontend/web/map_imgs/3.png" data-toggle="tooltip" title="Крив’янська сільська рада"/>
+             id="img3" src="http://tarashcha-rda.gov.ua/frontend/web/map_imgs/new/3.png" data-toggle="tooltip" title="Крив’янська сільська рада"/>
         <img class="map-img" data-href="<?=Url::to(['/site/article', 'slug' => 'saliska-silska-rada'])?>"
-             id="img4" src="http://tarashcha-rda.gov.ua/frontend/web/map_imgs/4.png" data-toggle="tooltip" title="Салиська сільська рада"/>
+             id="img4" src="http://tarashcha-rda.gov.ua/frontend/web/map_imgs/new/4.png" data-toggle="tooltip" title="Салиська сільська рада"/>
         <img class="map-img" data-href="<?=Url::to(['/site/article', 'slug' => 'cerninska-silska-rada'])?>"
-             id="img5" src="http://tarashcha-rda.gov.ua/frontend/web/map_imgs/5.png" data-toggle="tooltip" title="Чернинська сільська рада"/>
+             id="img5" src="http://tarashcha-rda.gov.ua/frontend/web/map_imgs/new/5.png" data-toggle="tooltip" title="Чернинська сільська рада"/>
         <img class="map-img" data-href="<?=Url::to(['/site/article', 'slug' => 'severinivska-silska-rada'])?>"
-             id="img6" src="http://tarashcha-rda.gov.ua/frontend/web/map_imgs/6.png" data-toggle="tooltip" title="Северинівська сільська рада"/>
+             id="img6" src="http://tarashcha-rda.gov.ua/frontend/web/map_imgs/new/6.png" data-toggle="tooltip" title="Северинівська сільська рада"/>
         <img class="map-img" data-href="<?=Url::to(['/site/article', 'slug' => 'rizkivska-silska-rada'])?>"
-             id="img7" src="http://tarashcha-rda.gov.ua/frontend/web/map_imgs/7.png" data-toggle="tooltip" title="Ріжківська сільська рада"/>
+             id="img7" src="http://tarashcha-rda.gov.ua/frontend/web/map_imgs/new/7.png" data-toggle="tooltip" title="Ріжківська сільська рада"/>
         <img class="map-img" data-href="<?=Url::to(['/site/article', 'slug' => 'volodimirivska-silska-rada'])?>"
-             id="img8" src="http://tarashcha-rda.gov.ua/frontend/web/map_imgs/8.png" data-toggle="tooltip" title="Володимирівська сільська рада"/>
+             id="img8" src="http://tarashcha-rda.gov.ua/frontend/web/map_imgs/new/8.png" data-toggle="tooltip" title="Володимирівська сільська рада"/>
         <img class="map-img" data-href="<?=Url::to(['/site/article', 'slug' => 'lisovicka-silska-rada'])?>"
-             id="img9" src="http://tarashcha-rda.gov.ua/frontend/web/map_imgs/9.png" data-toggle="tooltip" title="Лісовицька сільська рада"/>
+             id="img9" src="http://tarashcha-rda.gov.ua/frontend/web/map_imgs/new/9.png" data-toggle="tooltip" title="Лісовицька сільська рада"/>
         <img class="map-img" data-href="<?=Url::to(['/site/article', 'slug' => 'stepkivska-silska-rada'])?>"
-             id="img10" src="http://tarashcha-rda.gov.ua/frontend/web/map_imgs/10.png" data-toggle="tooltip" title="Степківська сільська рада"/>
+             id="img10" src="http://tarashcha-rda.gov.ua/frontend/web/map_imgs/new/10.png" data-toggle="tooltip" title="Степківська сільська рада"/>
         <img class="map-img" data-href="<?=Url::to(['/site/article', 'slug' => 'lukanivska-silska-rada'])?>"
-             id="img11" src="http://tarashcha-rda.gov.ua/frontend/web/map_imgs/11.png" data-toggle="tooltip" title="Лук’янівська сільська рада"/>
+             id="img11" src="http://tarashcha-rda.gov.ua/frontend/web/map_imgs/new/11.png" data-toggle="tooltip" title="Лук’янівська сільська рада"/>
         <img class="map-img" data-href="<?=Url::to(['/site/article', 'slug' => 'kislivska-silska-rada'])?>"
-             id="img12" src="http://tarashcha-rda.gov.ua/frontend/web/map_imgs/12.png" data-toggle="tooltip" title="Кислівська сільська рада"/>
+             id="img12" src="http://tarashcha-rda.gov.ua/frontend/web/map_imgs/new/12.png" data-toggle="tooltip" title="Кислівська сільська рада"/>
         <img class="map-img" data-href="<?=Url::to(['/site/article', 'slug' => 'lucanska-silska-rada'])?>"
-             id="img13" src="http://tarashcha-rda.gov.ua/frontend/web/map_imgs/13.png" data-toggle="tooltip" title="Лучанська сільська рада"/>
+             id="img13" src="http://tarashcha-rda.gov.ua/frontend/web/map_imgs/new/13.png" data-toggle="tooltip" title="Лучанська сільська рада"/>
         <img class="map-img" data-href="<?=Url::to(['/site/article', 'slug' => 'kivsovatska-silska-rada'])?>"
-             id="img14" src="http://tarashcha-rda.gov.ua/frontend/web/map_imgs/14.png" data-toggle="tooltip" title="Ківшоватська сільська рада"/>
+             id="img14" src="http://tarashcha-rda.gov.ua/frontend/web/map_imgs/new/14.png" data-toggle="tooltip" title="Ківшоватська сільська рада"/>
         <img class="map-img" data-href="<?=Url::to(['/site/article', 'slug' => 'stanisivska-silska-rada'])?>"
-             id="img15" src="http://tarashcha-rda.gov.ua/frontend/web/map_imgs/15.png" data-toggle="tooltip" title="Станишівська сільська рада"/>
+             id="img15" src="http://tarashcha-rda.gov.ua/frontend/web/map_imgs/new/15.png" data-toggle="tooltip" title="Станишівська сільська рада"/>
         <img class="map-img" data-href="<?=Url::to(['/site/article', 'slug' => 'velikovovnanska-silska-rada'])?>"
-             id="img16" src="http://tarashcha-rda.gov.ua/frontend/web/map_imgs/16.png" data-toggle="tooltip" title="Великововнянська сільська рада"/>
+             id="img16" src="http://tarashcha-rda.gov.ua/frontend/web/map_imgs/new/16.png" data-toggle="tooltip" title="Великововнянська сільська рада"/>
         <img class="map-img" data-href="<?=Url::to(['/site/article', 'slug' => 'petrivska-silska-rada'])?>"
-             id="img17" src="http://tarashcha-rda.gov.ua/frontend/web/map_imgs/17.png" data-toggle="tooltip" title="Петрівська сільська рада"/>
+             id="img17" src="http://tarashcha-rda.gov.ua/frontend/web/map_imgs/new/17.png" data-toggle="tooltip" title="Петрівська сільська рада"/>
         <img class="map-img" data-href="<?=Url::to(['/site/article', 'slug' => 'ploskivska-silska-rada'])?>"
-             id="img18" src="http://tarashcha-rda.gov.ua/frontend/web/map_imgs/18.png" data-toggle="tooltip" title="Плосківська сільська рада"/>
+             id="img18" src="http://tarashcha-rda.gov.ua/frontend/web/map_imgs/new/18.png" data-toggle="tooltip" title="Плосківська сільська рада"/>
         <img class="map-img" data-href="<?=Url::to(['/site/article', 'slug' => 'kalinovecka-silska-rada'])?>"
-             id="img19" src="http://tarashcha-rda.gov.ua/frontend/web/map_imgs/19.png" data-toggle="tooltip" title="Калиновецька сільська рада"/>
+             id="img19" src="http://tarashcha-rda.gov.ua/frontend/web/map_imgs/new/19.png" data-toggle="tooltip" title="Калиновецька сільська рада"/>
         <img class="map-img" data-href="<?=Url::to(['/site/article', 'slug' => 'dubivska-silska-rada'])?>"
-             id="img20" src="http://tarashcha-rda.gov.ua/frontend/web/map_imgs/20.png" data-toggle="tooltip" title="Дубівська сільська рада"/>
+             id="img20" src="http://tarashcha-rda.gov.ua/frontend/web/map_imgs/new/20.png" data-toggle="tooltip" title="Дубівська сільська рада"/>
         <img class="map-img" data-href="<?=Url::to(['/site/article', 'slug' => 'velikoberezanska-silska-rada'])?>"
-             id="img21" src="http://tarashcha-rda.gov.ua/frontend/web/map_imgs/21.png" data-toggle="tooltip" title="Великоберезянська сільська рада"/>
+             id="img21" src="http://tarashcha-rda.gov.ua/frontend/web/map_imgs/new/21.png" data-toggle="tooltip" title="Великоберезянська сільська рада"/>
         <img class="map-img" data-href="<?=Url::to(['/site/article', 'slug' => 'krutogorbivska-silska-rada'])?>"
-             id="img22" src="http://tarashcha-rda.gov.ua/frontend/web/map_imgs/22.png" data-toggle="tooltip" title="Крутогорбівська сільська рада"/>
+             id="img22" src="http://tarashcha-rda.gov.ua/frontend/web/map_imgs/new/22.png" data-toggle="tooltip" title="Крутогорбівська сільська рада"/>
         <img class="map-img" data-href="<?=Url::to(['/site/article', 'slug' => 'kosakivska-silska-rada'])?>"
-             id="img23" src="http://tarashcha-rda.gov.ua/frontend/web/map_imgs/23.png" data-toggle="tooltip" title="Косяківська сільська рада"/>
+             id="img23" src="http://tarashcha-rda.gov.ua/frontend/web/map_imgs/new/23.png" data-toggle="tooltip" title="Косяківська сільська рада"/>
         <img class="map-img" data-href="<?=Url::to(['/site/article', 'slug' => 'veselokutska-silska-rada'])?>"
-             id="img24" src="http://tarashcha-rda.gov.ua/frontend/web/map_imgs/24.png" data-toggle="tooltip" title="Веселокутська сільська рада"/>
+             id="img24" src="http://tarashcha-rda.gov.ua/frontend/web/map_imgs/new/24.png" data-toggle="tooltip" title="Веселокутська сільська рада"/>
     </div>
 </div>
